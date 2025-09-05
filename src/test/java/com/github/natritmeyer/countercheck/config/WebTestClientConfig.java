f934897e46680d1ab036bf48493c2234f0e78ef5
Nat Ritmeyer <217265+natritmeyer@users.noreply.github.com>
2025-09-05 13:59:16 +0100

    adding first preflight check with necessary dependencies, logging config, dependency injection

---
+++ src/test/java/com/github/natritmeyer/countercheck/config/WebTestClientConfig.java
@@ -0,0 +1,26 @@
+package com.github.natritmeyer.countercheck.config;
+
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+@PropertySource("classpath:/countercheck.properties")
+public class WebTestClientConfig {
+  @Bean
+  public WebTestClient getWebTestClient(@Value("${countercheck.request.timeout}") int timeoutSeconds) {
+    HttpClient httpClient = HttpClient.create()
+        .responseTimeout(Duration.ofSeconds(timeoutSeconds));
+
+    return WebTestClient
+        .bindToServer()
+        .responseTimeout(Duration.ofSeconds(timeoutSeconds))
+        .clientConnector(new ReactorClientHttpConnector(httpClient))
+        .build();
+  }
+}
--- src/test/java/com/github/natritmeyer/countercheck/preflightchecks/PreflightChecksTest.java
+++ src/test/java/com/github/natritmeyer/countercheck/preflightchecks/PreflightChecksTest.java
@@ -1,10 +1,52 @@
 package com.github.natritmeyer.countercheck.preflightchecks;
 
+import com.github.natritmeyer.countercheck.config.WebTestClientConfig;
+import java.time.Duration;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
+@SpringJUnitConfig(WebTestClientConfig.class)
 public class PreflightChecksTest {
-  @Test
-  public void waitForSomeTestEnvironmentPrecondition() {
+  private final WebTestClient webTestClient;
+  private final String customerServiceScheme;
+  private final String customerServiceHost;
+  private final int customerServicePort;
+  private final String customerServiceLivenessCheckPath;
 
+  @Autowired
+  public PreflightChecksTest(WebTestClient webTestClient,
+                             @Value("${countercheck.aut.customer-service.scheme}") String customerServiceScheme,
+                             @Value("${countercheck.aut.customer-service.host}") String customerServiceHost,
+                             @Value("${countercheck.aut.customer-service.port}") int customerServicePort,
+                             @Value("${countercheck.aut.customer-service.liveness-check-path}") String customerServiceLivenessCheckPath) {
+    this.webTestClient = webTestClient;
+    this.customerServiceScheme = customerServiceScheme;
+    this.customerServiceHost = customerServiceHost;
+    this.customerServicePort = customerServicePort;
+    this.customerServiceLivenessCheckPath = customerServiceLivenessCheckPath;
+  }
+
+  @Test
+  public void waitForCustomerService() {
+    Awaitility.await("Wait for Customer Service")
+        .pollInterval(Duration.ofSeconds(3))
+        .until(() ->
+            webTestClient
+                .get()
+                .uri(builder -> builder
+                    .scheme(customerServiceScheme)
+                    .host(customerServiceHost)
+                    .port(customerServicePort)
+                    .path(customerServiceLivenessCheckPath)
+                    .build())
+                .exchange()
+                .returnResult(Object.class)
+                .getStatus()
+                .value() == 200
+        );
   }
 }
