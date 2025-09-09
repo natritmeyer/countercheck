dc32d3326319fe930c8319d6fc952836c1384075
Nat Ritmeyer <217265+natritmeyer@users.noreply.github.com>
2025-09-09 11:08:59 +0100

    fixing typo

--- src/test/java/com/github/natritmeyer/countercheck/preflightchecks/VetsServicePreflightChecksTest.java
+++ src/test/java/com/github/natritmeyer/countercheck/preflightchecks/VetsServicePreflightChecksTest.java
@@ -17,32 +17,32 @@ public class VetsServicePreflightChecksTest {
   private final WebTestClient webTestClient;
   private final int pollForSeconds;
   private final int pollingIntervalSeconds;
-  private final String visitsServiceScheme;
-  private final String visitsServiceHost;
-  private final int visitsServicePort;
-  private final String visitsServiceLivenessCheckPath;
+  private final String vetsServiceScheme;
+  private final String vetsServiceHost;
+  private final int vetsServicePort;
+  private final String vetsServiceLivenessCheckPath;
 
   @Autowired
   public VetsServicePreflightChecksTest(WebTestClient webTestClient,
                                         @Value("${countercheck.preflightchecks.poll-for-seconds}") int pollForSeconds,
                                         @Value("${countercheck.preflightchecks.polling-interval-seconds}") int pollingIntervalSeconds,
-                                        @Value("${countercheck.aut.visits-service.scheme}") String visitsServiceScheme,
-                                        @Value("${countercheck.aut.visits-service.host}") String visitsServiceHost,
-                                        @Value("${countercheck.aut.visits-service.port}") int visitsServicePort,
-                                        @Value("${countercheck.aut.visits-service.liveness-check-path}") String visitsServiceLivenessCheckPath
+                                        @Value("${countercheck.aut.vets-service.scheme}") String vetsServiceScheme,
+                                        @Value("${countercheck.aut.vets-service.host}") String vetsServiceHost,
+                                        @Value("${countercheck.aut.vets-service.port}") int vetsServicePort,
+                                        @Value("${countercheck.aut.vets-service.liveness-check-path}") String vetsServiceLivenessCheckPath
   ) {
     this.webTestClient = webTestClient;
     this.pollForSeconds = pollForSeconds;
     this.pollingIntervalSeconds = pollingIntervalSeconds;
-    this.visitsServiceScheme = visitsServiceScheme;
-    this.visitsServiceHost = visitsServiceHost;
-    this.visitsServicePort = visitsServicePort;
-    this.visitsServiceLivenessCheckPath = visitsServiceLivenessCheckPath;
+    this.vetsServiceScheme = vetsServiceScheme;
+    this.vetsServiceHost = vetsServiceHost;
+    this.vetsServicePort = vetsServicePort;
+    this.vetsServiceLivenessCheckPath = vetsServiceLivenessCheckPath;
   }
 
   @Test
-  public void waitForVisitsService() {
-    Awaitility.await("Wait for Visits Service")
+  public void waitForVetsService() {
+    Awaitility.await("Wait for Vets Service")
         .atMost(Duration.ofSeconds(pollForSeconds))
         .pollInterval(Duration.ofSeconds(pollingIntervalSeconds))
         .ignoreException(WebClientRequestException.class)
@@ -51,11 +51,10 @@ public class VetsServicePreflightChecksTest {
             webTestClient
                 .get()
                 .uri(builder -> builder
-                    .scheme(visitsServiceScheme)
-                    .host(visitsServiceHost)
-                    .port(visitsServicePort)
-                    .path(visitsServiceLivenessCheckPath)
-                    .queryParam("petId", "1")
+                    .scheme(vetsServiceScheme)
+                    .host(vetsServiceHost)
+                    .port(vetsServicePort)
+                    .path(vetsServiceLivenessCheckPath)
                     .build())
                 .exchange()
                 .returnResult(Object.class)
