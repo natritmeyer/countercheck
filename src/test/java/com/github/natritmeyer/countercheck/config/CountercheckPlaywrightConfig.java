8cb1a89d822eb292610c3b9452536a92c8180b07
Nat Ritmeyer <217265+natritmeyer@users.noreply.github.com>
2025-10-15 11:37:31 +0100

    adding proxy capability to playwright browser

--- src/test/java/com/github/natritmeyer/countercheck/config/CountercheckPlaywrightConfig.java
+++ src/test/java/com/github/natritmeyer/countercheck/config/CountercheckPlaywrightConfig.java
@@ -1,12 +1,16 @@
 package com.github.natritmeyer.countercheck.config;
 
+import com.github.natritmeyer.countercheck.support.network.ProxyManager;
 import com.github.natritmeyer.countercheck.support.playwright.CountercheckPlaywrightBrowserType;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.Proxy;
 import java.nio.file.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -22,13 +26,16 @@ import org.springframework.context.annotation.PropertySource;
 })
 @PropertySource(value = {"classpath:/playwright.properties"})
 public class CountercheckPlaywrightConfig {
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
+
   private final Playwright playwright;
   private final Browser browser;
   private final BrowserContext browserContext;
   private final Page page;
 
   @Autowired
-  public CountercheckPlaywrightConfig(@Value("${countercheck.playwright.browser-type}") String playwrightBrowserTypeName) {
+  public CountercheckPlaywrightConfig(@Value("${countercheck.playwright.browser-type}") String playwrightBrowserTypeName,
+                                      ProxyManager proxyManager) {
     Playwright.CreateOptions createOptions = new Playwright.CreateOptions();
     this.playwright = Playwright.create(createOptions);
 
@@ -36,6 +43,18 @@ public class CountercheckPlaywrightConfig {
         .parse(playwrightBrowserTypeName)
         .getBrowserType(playwright);
     BrowserType.LaunchOptions launchOptions = new BrowserType.LaunchOptions();
+
+    log.debug("Proxy config: {}", proxyManager.toString());
+    if (proxyManager.proxyRequired()) {
+      String proxy = String.format("http://%s:%d",
+          proxyManager.getProxyHost(),
+          proxyManager.getProxyPort());
+
+      launchOptions.setProxy(
+          new Proxy(proxy)
+              .setUsername(proxyManager.getProxyUsername())
+              .setPassword(proxyManager.getProxyPassword()));
+    }
     this.browser = browserType.launch(launchOptions);
 
     Browser.NewContextOptions newContextOptions = new Browser.NewContextOptions();
