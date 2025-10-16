d52b7e157e9e0fb054239b354a8bc8ef51ca95e0
Nat Ritmeyer <217265+natritmeyer@users.noreply.github.com>
2025-10-16 11:17:35 +0100

    moving environment specific config to relevant properties files, injecting url to home page object

--- src/test/java/com/github/natritmeyer/countercheck/model/pages/HomePage.java
+++ src/test/java/com/github/natritmeyer/countercheck/model/pages/HomePage.java
@@ -2,19 +2,25 @@ package com.github.natritmeyer.countercheck.model.pages;
 
 import com.microsoft.playwright.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HomePage {
   private final Page page;
+  private final String url;
 
   @Autowired
-  public HomePage(Page page) {
+  public HomePage(Page page,
+                  @Value("${countercheck.aut.ui-service.scheme}") String scheme,
+                  @Value("${countercheck.aut.ui-service.host}") String host,
+                  @Value("${countercheck.aut.ui-service.port}") int port) {
     this.page = page;
+    this.url = String.format("%s://%s:%d", scheme, host, port);
   }
 
   public void load() {
-    this.page.navigate("http://localhost:8080");
+    this.page.navigate(url);
   }
 
   public void navigateToRegisterNewOwnerPage() {
